a5255d57928cd3f36e05b20e813cc91a51294ccf
= <=>
2021-06-19 14:32:49 -0300

    Exercício sobre DateTime

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            DateTime agora = DateTime.Now;
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("=>");
+            Console.ResetColor();
+
+            Console.Write(" A data e hora nesse momento são respectivamente: ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"{agora}");
+            Console.ResetColor();
+
+        }
+    }
+}
